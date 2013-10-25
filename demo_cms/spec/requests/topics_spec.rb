6a04918efa23722277242fc48d3571b96d40a95f
greatghoul <greatghoul@gmail.com>
2013-10-25 16:30:31 +0800

    Add two basic test for topics

--- demo_cms/spec/requests/topics_spec.rb
+++ demo_cms/spec/requests/topics_spec.rb
@@ -2,10 +2,18 @@ require 'spec_helper'
 
 describe "Topics" do
   describe "GET /topics" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+    it "Display topics" do
+      Topic.create!(title: 'Hello World!', content: 'Hello World!')
       get topics_path
-      response.status.should be(200)
+      response.body.should include('Hello World!')
+    end
+  end
+
+  describe "POST /topics" do
+    it "Create topic" do
+      Topic.create!(title: 'Hello World!', content: 'Hello World!')
+      post_via_redirect topics_path, topic: { title: 'topic1', content: 'content 1' }
+      response.body.should include('topic1')
     end
   end
 end
