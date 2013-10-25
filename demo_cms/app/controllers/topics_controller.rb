955c29d289ee4a50c33dcaeb4d9f7eba8e975e45
greatghoul <greatghoul@gmail.com>
2013-10-26 00:12:41 +0800

    Add permission check for topic actions

--- demo_cms/app/controllers/topics_controller.rb
+++ demo_cms/app/controllers/topics_controller.rb
@@ -1,4 +1,7 @@
 class TopicsController < ApplicationController
+  before_filter :authenticate_user!, except: [:index, :show]
+  load_and_authorize_resource
+
   # GET /topics
   # GET /topics.json
   def index
